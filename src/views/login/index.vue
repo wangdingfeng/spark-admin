d29a368b675c8aa69210272c5a83ef9ee1eae9f8
wangdingfeng <wangdingfeng@live.com>
2020-03-17 17:15:12 +0800

    修改登陆成功提示

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -59,6 +59,10 @@ export default {
           this.$store.dispatch('Login', this.loginForm).then(() => {
             this.loading = false
             this.$router.push({ path: '/' })
+            this.$message({
+              message: '登录成功，请使用',
+              type: 'success'
+            })
           }).catch(() => {
             this.loading = false
           })
